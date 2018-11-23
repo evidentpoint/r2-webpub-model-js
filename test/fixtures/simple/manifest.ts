a5a1d87c8b483b1618717cc558f789c3b9b6f510
Juan Corona <juan.carlos.corona.r@gmail.com>
2018-11-23 14:16:22 -0800

    Update test manifests

--- test/fixtures/simple/manifest.ts
+++ test/fixtures/simple/manifest.ts
@@ -6,37 +6,63 @@ export default {
     title: 'Moby-Dick',
     modified: '2015-09-29T17:00:00.000Z',
     language: 'en',
-    author: { name: 'Herman Melville' },
+    author: {
+      name: 'Herman Melville',
+    },
   },
   links: [
-    { rel: 'self', href: 'https://example.com/manifest.json', type: 'application/webpub+json' },
     {
-      rel: 'alternate',
-      href: 'https://example.com/publication.epub',
-      type: 'application/epub+zip',
+      href: 'https://example.com/manifest.json',
+      type: 'application/webpub+json',
+      rel: 'self',
     },
     {
-      rel: 'search',
+      href: 'https://example.com/publication.epub',
+      type: 'application/epub+zip',
+      rel: 'alternate',
+    },
+    {
       href: 'https://example.com/search{?query}',
       type: 'text/html',
+      rel: 'search',
       templated: true,
     },
   ],
   readingOrder: [
-    { href: 'https://example.com/c001.html', type: 'text/html', title: 'Chapter 1' },
-    { href: 'https://example.com/c002.html', type: 'text/html', title: 'Chapter 2' },
+    {
+      href: 'https://example.com/c001.html',
+      type: 'text/html',
+      title: 'Chapter 1',
+    },
+    {
+      href: 'https://example.com/c002.html',
+      type: 'text/html',
+      title: 'Chapter 2',
+    },
   ],
   resources: [
     {
-      rel: 'cover',
       href: 'https://example.com/cover.jpg',
       type: 'image/jpeg',
+      rel: 'cover',
       height: 600,
       width: 400,
     },
-    { href: 'https://example.com/style.css', type: 'text/css' },
-    { href: 'https://example.com/whale.jpg', type: 'image/jpeg' },
-    { href: 'https://example.com/boat.svg', type: 'image/svg+xml' },
-    { href: 'https://example.com/notes.html', type: 'text/html' },
+    {
+      href: 'https://example.com/style.css',
+      type: 'text/css',
+    },
+    {
+      href: 'https://example.com/whale.jpg',
+      type: 'image/jpeg',
+    },
+    {
+      href: 'https://example.com/boat.svg',
+      type: 'image/svg+xml',
+    },
+    {
+      href: 'https://example.com/notes.html',
+      type: 'text/html',
+    },
   ],
 };
